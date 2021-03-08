ab5bd4bc4aa7bdb5351cb3bf9398d72c1e2b9b08
Danh Nguyễn <nguyentd92@gmail.com>
2021-03-08 22:49:03 +0700

    Account Model: Declare constructor & method findByEmailAndPassword

--- watch-shop-php-mvc/src/models/account.php
+++ watch-shop-php-mvc/src/models/account.php
@@ -6,4 +6,36 @@ class Account {
     public $email;
     public $password;
     public $last_login_at;
+
+    public function __construct(
+        $account_id,
+        $full_name,
+        $email,
+        $password = null,
+        $last_login_at = null
+    ) {
+        $this->account_id = $account_id;
+        $this->email = $email;
+        $this->full_name = $full_name;
+        $this->password = $password;
+        $this->last_login_at = $last_login_at;
+    }
+
+    static function findByEmailAndPassword($email, $password) {
+        $db = DbConnection::getInstance();
+        $query = "SELECT * FROM users WHERE email = ? AND password = ?";
+        $req = $db->query($query, [$email, $password]);
+        $rawData = $req->fetch();
+        if(isset($rawData)) {
+            return new Account(
+                $rawData["account_id"],
+                $rawData["full_name"],
+                $rawData["email"],
+                $rawData["pasword"],
+                $rawData["last_login_at"]
+            );
+        }
+
+        return null;
+    }
 }
